037217240b90ce6f965d8fe6c5d4b248044bc80e
Dipti-C <130269466+Dipti-C@users.noreply.github.com>
2023-05-05 15:48:15 +0530

    Update Hello World.java

--- Hello World.java
+++ Hello World.java
@@ -3,6 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-		System.out.println("Hello, Dipti!"); 
+	System.out.println("Hello, Dipti!"); 
+	System.out.println("Hi, this is test");
     }
 }
